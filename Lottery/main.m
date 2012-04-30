afc30d2fbcff4cbdb7d130a7462e55176f51f24e
Pat Escalona <jellohead7000@gmail.com>
2012-04-30 11:46:11 +0800

    Initial Commit

---
+++ Lottery/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Lottery
+//
+//  Created by Pat Escalona on 4/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
